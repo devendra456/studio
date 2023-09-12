dc11b34a2bb83d41e6f092c602b02aebddfec57e
Devendra Maurya <devendramaurya456@gmail.com>
2023-09-12 21:35:38 +0530

    android and windows link added

--- lib/presentation/on_boarding/views/on_boarding_screen.dart
+++ lib/presentation/on_boarding/views/on_boarding_screen.dart
@@ -1,8 +1,10 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
 import 'package:studio/domain/entities/image_entity.dart';
 import 'package:studio/presentation/on_boarding/views/studio_grid_tile.dart';
+import 'package:url_launcher/url_launcher_string.dart';
 
 import '../../../application/routes/route_names.dart';
 import '../bloc/on_boarding_bloc.dart';
@@ -19,6 +21,28 @@ class OnBoardingScreen extends StatelessWidget {
           appBar: AppBar(
             title: const Text("Studio"),
             actions: [
+              if (kIsWeb)
+                IconButton(
+                  onPressed: () async {
+                    const url =
+                        "https://drive.google.com/file/d/1FN3pOiFz8DCnvwVv49tsL9FWIwK81JTY/view?usp=sharing";
+                    if (await canLaunchUrlString(url)) {
+                      await launchUrlString(url);
+                    }
+                  },
+                  icon: const Icon(Icons.android_outlined),
+                ),
+              if (kIsWeb)
+                IconButton(
+                  onPressed: () async {
+                    const url =
+                        "https://drive.google.com/file/d/1Nt3FQoeFWPLA4lvqU9kE6wL6WqzVo6BI/view?usp=sharing";
+                    if (await canLaunchUrlString(url)) {
+                      await launchUrlString(url);
+                    }
+                  },
+                  icon: const Icon(Icons.desktop_windows_outlined),
+                ),
               IconButton(
                 onPressed: () {
                   Navigator.pushNamed(context, RouteName.setting);
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,22 +9,22 @@ import connectivity_plus
 import dynamic_color
 import file_selector_macos
 import firebase_core
-import image_editor_common
 import pasteboard
 import path_provider_foundation
 import share_plus
 import shared_preferences_foundation
 import sqflite
+import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   ConnectivityPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlugin"))
   DynamicColorPlugin.register(with: registry.registrar(forPlugin: "DynamicColorPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
-  ImageEditorPlugin.register(with: registry.registrar(forPlugin: "ImageEditorPlugin"))
   PasteboardPlugin.register(with: registry.registrar(forPlugin: "PasteboardPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
